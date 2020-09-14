fe329ddb08a4fc29300b324bfd9d0fcf07ed8ac3
Arthur Lozano <chillout885@gmail.com>
2020-09-14 00:34:50 -0700

    updated more code

---
+++ js/prompt.js
@@ -0,0 +1,13 @@
+'use strict'; 
+
+
+var userAge = prompt ("Please enter your age")
+
+if (userAge === 17) 
+{
+    text = ("Sorry, you're too young!");
+} 
+else if (userAge > 17) {
+    text = ("Enter my site!");}
+    
+document.write(userAge);
