1a1e2029d0cf8dd6ab8f31211d5700b840c35540
Arthur Lozano <chillout885@gmail.com>
2020-09-15 02:25:51 -0700

    got javascript right

--- js/prompt.js
+++ js/prompt.js
@@ -1,3 +1,23 @@
+'use strict';
+
+var userAct = prompt ('Ready to have fun? ' +  'yes or no?');
+
+
+if (userAct == 'yes') {
+    document.write('<h2>' + 'Glad your answer was  ' + userAct + '<h2>');
+}
+
+else if (userAct == 'no') {
+    document.write('<h2>' + 'Sorry your answer was ' + userAct + '<h2>');
+}
+
+else {
+    document.write('<h2>' + 'Maybe next time! ' + '<h2>');
+}
+
+
+
+/*
 'use strict'; 
 
 
@@ -17,3 +37,4 @@ return document.write(userAge);}
 
 //Below calls the function
 wantIn();
+'*/
