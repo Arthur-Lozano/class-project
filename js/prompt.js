05e426497aa8e601758a377b1d23adeaebd61449
Arthur Lozano <chillout885@gmail.com>
2020-09-16 21:23:43 -0700

    updated java

--- js/prompt.js
+++ js/prompt.js
@@ -1,29 +1,54 @@
-'use strict';
+'use strict'
 
-function wannaParty () {
+//Filter Help Function
+function wannaParty(){
 
-var userAct = prompt ('Ready to have fun? ' +  'yes or no?');
-var answer;
-  
+  var userAct = prompt('please choose a number, choose 1 for amsterdam or 2 for the bush?');
+  var answer;
+    
 
-if (userAct == 'yes') {
-    answer = ('Glad your answer was YES');
-}
-
-else if (userAct == 'no') {
-    answer =  ('Sorry your answer was no');
-}
-
-else {
-    answer = 'Maybe next time!';
+  while(userAct != "1" && userAct != "2"){
+    userAct = prompt('Please enter a number 1 or 2 ONLY!'); // re assign value of user act to new prompt
   }
-  return document.write('<h1>'+ answer + '</h1');
+
+  if (userAct === "1") {
+      answer = '<img src = "../images/pic1.jpg">'; // insert pics
+  } else if (userAct === "2") {
+      answer =  '<img src = "../images/pic2.jpg">'; // insert pics
+  }
+  console.log("answer", answer);
+  return answer;
 }
 
- wannaParty();
+/* Prompt the user for how many things they’d like your shop to make.
 
+    Use a loop in your JavaScript to add items to the page that many times.
+    Can functions make your code more readable?
+*/
 
+//helper functions
 
+function getCount() {
+var count = prompt('Great!  Here you go!');
+while(isNaN(count) || count == '') {
+  count = prompt("Please enter a number.  Which picture would you like??");
+ }
+ return count;
+}
+
+//Where helper functions are being called - king of functions
+function showOrder() {
+  var result = '';
+  var itemType = wannaParty();
+  var total = getCount();
+
+  for(var i =0; i < total; i++) {
+    result = result + '<p>' + itemType + '</p>';
+  }
+return document.write(result);
+}
+
+showOrder();
 
 
 
